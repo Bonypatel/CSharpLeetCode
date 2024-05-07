9c2dc037d20f5a9f818438a8945ec608d51962f7
Bony Patel <bonyp96@gmail.com>
2024-05-07 15:10:14 -0400

    Adding C# solution for Leet code 1 and 2.

---
+++ LeetCode_1/Program.cs
@@ -0,0 +1,52 @@
+﻿/*
+Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+You may assume that each input would have exactly one solution, and you may not use the same element twice.
+You can return the answer in any order. 
+*/
+
+public class Solution {
+    public int[] TwoSum(int[] nums, int target) {
+        int[] ans = [0,0];
+        int left = 0;
+        int right = 1;
+
+        while (right < nums.Length)
+        {
+            Console.WriteLine("left = {0} and right = {1}", left, right);
+            Console.WriteLine("Sum of nums[left] + nums[right] = {0}", nums[left] + nums[right]);
+            if ( nums[left] + nums[right] == target)
+            {
+                ans[0] = left;
+                ans[1] = right;
+                right = nums.Length;
+                Console.WriteLine("It found the target!");
+            }
+            else if (right == nums.Length - 1)
+            {
+                left++;
+                right = left + 1;
+                Console.WriteLine("Right has hit nums.Length, left = {0} and right = {1}", left, right);
+            }
+            else
+            {
+                right++;
+                Console.WriteLine("Just iterating Right: Right = {0}", right);
+            }
+        }
+
+        return ans;
+    }
+
+    static void Main()
+    {
+        Solution mySolution = new Solution();
+        int[] test = [3,2,4];
+        int target = 6;
+        int[] solution = mySolution.TwoSum(test, target);
+        foreach (int num in solution)
+        {
+            Console.WriteLine(num);
+        }
+        
+    }
+}
---
+++ LeetCode_22/Program.cs
@@ -0,0 +1,46 @@
+﻿
+/*
+Given n pairs of parentheses, write a function to generate all combinations of well-formed parentheses.
+
+Rules:
+Only add onpen paranthesis if open_count < n
+only add a closing paranthesis if closed_count < open_count
+only a vailid answer if open_count == closed_count == n
+*/
+
+public class Solution {
+    public IList<string> GenerateParenthesis(int n) {
+        List<string> ans = new List<string>();
+        string testAnser = string.Empty;
+         
+        void BackTrack(int openCount,int closedCount,string path)
+        {
+            if (openCount == closedCount && openCount == n && closedCount == n) 
+            {
+                ans.Add(path);
+                return;
+            }
+            if (openCount < n)
+            {
+                BackTrack(openCount + 1, closedCount, path + "(");
+            }
+            if (closedCount < openCount)
+            {
+                BackTrack(openCount, closedCount + 1, path + ")");
+            }
+
+        }
+
+        BackTrack(0,0,"");
+
+        return ans;
+    }
+
+
+    static void Main()
+    {
+        Solution mySolution = new Solution();
+
+        mySolution.GenerateParenthesis(3);
+    }
+}
